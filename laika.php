10c5b04162eefb9d2fde5bc7d0069833ba6a8908
Ryan McCue <me@ryanmccue.info>
2012-01-10 18:30:42 +1000

    Initial commit

---
+++ laika.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Laika
+Description: An example plugin to show how to use the Sputnik API
+Version: 1.0
+Author: Ryan McCue
+Author URI: http://ryanmccue.info/
+*/
+
+class Laika {
+	public static function verify() {
+		remove_action('all_admin_notices', array('Laika', 'report_error'));
+		Sputnik::check(__FILE__, array('Laika', 'bootstrap'));
+	}
+
+	public static function report_error() {
+		echo '<div class="error"><p>Please install &amp; activate Sputnik to enable Laika.</p></div>';
+	}
+
+	public static function bootstrap() {
+		add_filter('update_footer', array(__CLASS__, 'footer'), 20);
+	}
+
+	public static function footer($text) {
+		$text = 'Dedicated to Laika (1954&ndash;1957) &bull; ' . $text;
+		return $text;
+	}
+}
+
+add_action('sputnik_loaded', array('Laika', 'verify'));
+add_action('all_admin_notices', array('Laika', 'report_error'));
---
+++ readme.md
@@ -0,0 +1,31 @@
+=== Laika ===  
+Contributors: rmccue  
+Tags: laika, sputnik, example, test  
+Requires at least: 3.2  
+Tested up to: 3.4  
+Stable tag: 0.1  
+
+Laika is a test plugin to show you how to use Sputnik's API.
+
+== Description ==
+
+Laika is a test plugin to show you how to use Sputnik's API.
+
+Laika is dedicated to the first dog in space, who gave her life to for
+the advancement of the human race.
+
+== Installation ==
+
+1. Purchase Laika through the app store
+2. Download and activate Laika
+3. Enjoy!
+
+== Frequently Asked Questions ==
+
+None yet!
+
+== Changelog ==
+
+= 0.1 =
+
+* Create plugin
